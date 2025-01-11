37b7db5c40e2fde372fcd037c09c1f76a0753fb9
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-01-11 14:19:03 +0530

    Remove custom error handling method from ApplicationService

--- app/services/application_service.rb
+++ app/services/application_service.rb
@@ -2,9 +2,4 @@ class ApplicationService
   def self.call(*args, &block)
     new(*args, &block).call
   end
-
-  # Optional: Override this to define a custom error handler in subclasses
-  def handle_error(error)
-    raise error
-  end
 end
