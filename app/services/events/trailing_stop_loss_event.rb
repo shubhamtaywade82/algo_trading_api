6eb3e0cb341b400433d34ea70a231e574f9cb4a6
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-01-24 17:10:20 +0530

    Remove unused service classes and WebSocket handlers to streamline codebase

--- app/services/events/trailing_stop_loss_event.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class TrailingStopLossEvent
-  def self.trigger(order_id, new_stop_loss)
-    ActiveSupport::Notifications.instrument('order.stop_loss_updated', order_id: order_id, new_stop_loss: new_stop_loss)
-  end
-end
