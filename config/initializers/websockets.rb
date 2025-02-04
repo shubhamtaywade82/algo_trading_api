d533aa626bc7a021015e6ca65e20141ccefd4af4
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-02-04 09:16:24 +0530

    Update Procfile.dev to use environment variable for port and comment out WebSocket initialization code

--- config/initializers/websockets.rb
+++ config/initializers/websockets.rb
@@ -1,19 +1,19 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-Thread.new do
-  # Start Live Market Feed WebSocket
-  begin
-    market_feed = LiveMarketFeed.new
-    market_feed.connect
-  rescue StandardError => e
-    Rails.logger.error "[WebSocket] Error starting LiveMarketFeed: #{e.message}"
-  end
+# Thread.new do
+#   # Start Live Market Feed WebSocket
+#   begin
+#     market_feed = LiveMarketFeed.new
+#     market_feed.connect
+#   rescue StandardError => e
+#     Rails.logger.error "[WebSocket] Error starting LiveMarketFeed: #{e.message}"
+#   end
 
-  # Start Live Order Update WebSocket
-  begin
-    order_update = LiveOrderUpdate.new
-    order_update.connect
-  rescue StandardError => e
-    Rails.logger.error "[WebSocket] Error starting LiveOrderUpdate: #{e.message}"
-  end
-end
+#   # Start Live Order Update WebSocket
+#   begin
+#     order_update = LiveOrderUpdate.new
+#     order_update.connect
+#   rescue StandardError => e
+#     Rails.logger.error "[WebSocket] Error starting LiveOrderUpdate: #{e.message}"
+#   end
+# end
