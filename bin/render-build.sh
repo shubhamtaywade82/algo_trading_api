b96f0e481cfea3950bb1bb0d4afbf731a7ab85e1
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-02-06 00:04:20 +0530

    Refactor render-build script to streamline database migrations and seeding process

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,12 +3,8 @@
 set -o errexit
 # Install gems
 bundle install
+rails db:migrate
+rails db:seed
 
-# On the free plan, there's NO "pre-deploy command," so run migrations here:
-bundle exec rails db:migrate
-# If you want to run seeds on every deploy, include:
-bundle exec rails db:seed
-
-# If you have custom rake tasks for data import:
-# bundle exec rails import:instruments
-# bundle exec rails import:mis_details
+rails import:instruments
+# rails import:mis_details
