41f9b6ba6ea3b0e099598b3e39fb7d970fcb20d8
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-02-06 00:22:47 +0530

    Update routes to include show action for instruments

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   get '/statements/ledger', to: 'statements#ledger'
   get '/statements/trade_history', to: 'statements#trade_history'
 
-  resources :instruments, only: [:index]
+  resources :instruments, only: %i[index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
