aa07f61b319ac68703262d05555042fe8c25c84f
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-02-07 16:14:25 +0530

    Comment out insufficient balance check in margin validation method

--- app/services/alert_processors/stock.rb
+++ app/services/alert_processors/stock.rb
@@ -84,7 +84,7 @@ module AlertProcessors
     def validate_margin(params)
       params = params.merge(price: instrument.ltp)
       response = Dhanhq::API::Funds.margin_calculator(params)
-      insufficient_balance = response['insufficientBalance'].to_f
+      # insufficient_balance = response['insufficientBalance'].to_f
 
       # raise "Insufficient margin: Missing ₹#{insufficient_balance}" if insufficient_balance.positive?
 
