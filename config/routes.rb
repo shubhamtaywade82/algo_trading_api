13fc86c5e99a3cc48440d804c0840b882c79f424
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-06-12 00:48:23 +0530

    Update root route to return a JSON response indicating service status instead of rendering posts index.

--- config/routes.rb
+++ config/routes.rb
@@ -37,5 +37,5 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root to: proc { [200, { 'Content-Type' => 'application/json' }, ['{"status":"ok"}']] }
 end
