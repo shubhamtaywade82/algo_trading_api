822ffd2384b4a7b26329ecde1e1a8337473bdb53
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-07-26 14:45:49 +0530

    Refactor cron job start command for technical analysis; improve readability by using a multi-line bash command

--- db/schema.rb
+++ db/schema.rb
@@ -186,7 +186,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_26_072221) do
     t.decimal "sell_bo_profit_range_min_perc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["requirementable_type", "requirementable_id"], name: "index_margin_requirements_on_requirementable", unique: true
+    t.index ["requirementable_type", "requirementable_id"], name: "index_margin_requirements_on_requirementable"
   end
 
   create_table "mis_details", force: :cascade do |t|
@@ -198,16 +198,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_26_072221) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["instrument_id"], name: "index_mis_details_on_instrument_id"
-  end
-
-  create_table "ohlcv_blobs", force: :cascade do |t|
-    t.string "symbol"
-    t.string "timeframe"
-    t.date "day"
-    t.jsonb "data"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["symbol", "timeframe", "day"], name: "index_ohlcv_blobs_on_symbol_and_timeframe_and_day", unique: true
   end
 
   create_table "order_features", force: :cascade do |t|
