e5353af98f9622c4784ff019bd9d53b378ab405c
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-07-28 16:42:22 +0530

    Add time-based condition for UpdateTechnicalAnalysisJob execution in TA loop

--- config/initializers/ta_scheduler.rb
+++ config/initializers/ta_scheduler.rb
@@ -1,9 +1,11 @@
+START_TIME = Time.zone.parse('09:10')
+END_TIME = Time.zone.parse('15:30')
 if ENV['ENABLE_TA_LOOP'] == 'true'
   Rails.application.config.after_initialize do
     Thread.new do
       loop do
         # Loads fine because autoloading is finished
-        UpdateTechnicalAnalysisJob.perform_later
+        UpdateTechnicalAnalysisJob.perform_later if Time.zone.now > START_TIME && Time.zone.now < END_TIME
         sleep 5.minutes
       end
     end
