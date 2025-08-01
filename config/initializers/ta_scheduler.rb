7c4a44842151a8576b17089c39e952a4bd13c304
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-08-01 09:30:15 +0530

    Update ta_scheduler.rb

--- config/initializers/ta_scheduler.rb
+++ config/initializers/ta_scheduler.rb
@@ -6,7 +6,7 @@ if ENV['ENABLE_TA_LOOP'] == 'true'
       loop do
         # Loads fine because autoloading is finished
         UpdateTechnicalAnalysisJob.perform_later if Time.zone.now > START_TIME && Time.zone.now < END_TIME
-        sleep 5.minutes
+        sleep 3.minutes
       end
     end
   end
