066737fc482a6decd74b6ac7c363516206da2d0e
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-08-31 11:55:27 +0530

    chore: remove legacy Managers, old jobs, and unused trading_services

--- app/jobs/order_manager_job.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class OrderManagerJob < ApplicationJob
-  queue_as :default
-
-  def perform
-    Managers::Orders::Processor.call
-  end
-end
--- app/jobs/positions_manager_job.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class PositionsManagerJob < ApplicationJob
-  queue_as :default
-
-  def perform
-    Managers::Positions.call
-  end
-end
