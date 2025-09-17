9792fceb4a82896bda389b4f4e126ffcd60dc377
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-09-17 23:12:49 +0530

    Update market_analysis_job.rb

--- app/jobs/market_analysis_job.rb
+++ app/jobs/market_analysis_job.rb
@@ -2,7 +2,7 @@ class MarketAnalysisJob < ApplicationJob
   queue_as :default
 
   def perform(chat_id, symbol, exchange: :nse, trade_type: nil)
-    answer = Market::AnalysisService.call(symbol, exchange: exchange, trade_type: trad_type)
+    answer = Market::AnalysisService.call(symbol, exchange: exchange, trade_type: trade_type)
     TelegramNotifier.send_message(answer, chat_id: chat_id) if answer.present?
   rescue StandardError => e
     Rails.logger.error "[MarketAnalysisJob] ❌ #{e.class} – #{e.message}"
